33ac393fee7a0c25f783a159f8739fb093d4fa03
EduardoHFreitas <eduardo.freitas@datacoper.com.br>
2016-04-24 12:15:09 -0300

    Criação dos metodos na classe Principal

--- src/br/univel/view/Principal.java
+++ src/br/univel/view/Principal.java
@@ -1,12 +1,59 @@
 package br.univel.view;
 
-public class Principal {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import br.univel.abstratc.SqlGen;
+
+public class Principal extends SqlGen {
 
 	public Principal(){
-		
+
 	}
-	
+
 	public static void main (String[] args){
 		new Principal();
+	}
+
+	@Override
+	protected String getCreateTable(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getDropTable(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlInsert(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlSelectAll(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlSelectById(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlUpdateById(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected PreparedStatement getSqlDeleteById(Connection con, Object obj) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
