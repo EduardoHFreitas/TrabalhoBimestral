a976b64b625ece7370393d2be08e0a119838d5d0
EduardoHFreitas <eduardo.freitas@datacoper.com.br>
2016-04-24 23:51:43 -0300

    Criação da assinatura da inteface

--- src/br/univel/interfaces/Funcoes.java
+++ src/br/univel/interfaces/Funcoes.java
@@ -1,5 +1,17 @@
 package br.univel.interfaces;
 
-public interface Funcoes {
+import java.util.List;
+import br.univel.view.Cliente;
+
+public interface Funcoes <obj, id> {
+	public void salvar(obj id);
+
+	public obj buscar(obj id);
+
+	public void atualizar(obj id);
+
+	public void excluir(obj id);
+
+	public List<Cliente> listarTodos();
 
 }
